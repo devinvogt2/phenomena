19b863c99b67bf0d78f28bd986d23000c1192246
devinvogt2 <vogtdevin@ymail.com>
2022-01-24 20:52:17 -0600

    make it better

--- db/index.js
+++ db/index.js
@@ -4,7 +4,7 @@ const CONNECTION_STRING =
   process.env.DATABASE_URL || "postgres://localhost:5432/phenomena-dev";
 // Create the client using new Client(CONNECTION_STRING)
 // Do not connect to the client in this file!
-const client = new Client(CONNECTION_STRING);
+const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/phenomena-dev');
 
 // Require the Client constructor from the pg package
 async function getOpenReports() {
